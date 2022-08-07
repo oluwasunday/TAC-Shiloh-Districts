dee5cd001e50b1e8db591aec22daa9098a58a663
Oladejo Sunday <oladejo.sunday@giglogistics.ng>
2022-08-07 13:59:04 +0100

    prayer request entity and social media added

---
+++ TACShilohDistricts.Core/Entities/PrayerRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TACShilohDistricts.Core.Entities
+{
+    public class PrayerRequest : BaseEntity
+    {
+        public new int Id { get; set; } 
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress (ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength (100)]
+        public string Subject { get; set; }
+        [Required]
+        public string Request { get; set; }
+    }
+}
