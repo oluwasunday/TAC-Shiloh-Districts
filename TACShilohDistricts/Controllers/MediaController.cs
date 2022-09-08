378a8fff7c90312ff5d336fb6a6dfa1aae74ae26
Oladejo Sunday <oladejo.sunday@giglogistics.ng>
2022-09-08 10:09:50 +0100

    Added Media controller with Gallery and VideoStream views

---
+++ TACShilohDistricts/Controllers/MediaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TACShilohDistricts.Controllers
+{
+    public class MediaController : Controller
+    {
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+        
+        public IActionResult VideoStream()
+        {
+            return View();
+        }
+    }
+}
