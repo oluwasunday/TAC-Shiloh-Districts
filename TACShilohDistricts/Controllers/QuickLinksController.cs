c4869f787e0440768ecc26a25390e4ea10a0877f
Oladejo Sunday <oladejo.sunday@giglogistics.ng>
2022-09-30 06:39:34 +0100

    prayer request, testimonies and news events grouped together

--- TACShilohDistricts/Controllers/QuickLinksController.cs
+++ TACShilohDistricts/Controllers/QuickLinksController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TACShilohDistricts.Core.IServices;
+using TACShilohDistricts.Core.ViewModel;
 
 namespace TACShilohDistricts.Controllers
 {
     public class QuickLinksController : Controller
     {
+        private readonly ITestimonyService _testimonyService;
+
+        public QuickLinksController(ITestimonyService testimonyService)
+        {
+            _testimonyService = testimonyService;
+        }
+
         public IActionResult NewsAndEvents()
         {
             return View();
+        }
+
+        public IActionResult PrayerRequest()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> TestimoniesAsync()
+        {
+            var testimonies = await _testimonyService.GetAllTestimoniesAsync();
+            var vm = new TestimoniesViewModel
+            {
+                Testimonies = testimonies
+            };
+
+            if (vm == null)
+            {
+                TempData["error"] = "error occur";
+            }
+            return View(vm);
+            //return View();
         }
     }
 }
